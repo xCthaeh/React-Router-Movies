36761a9bbc15868800711d30e8baa2de52fa2393
alyssa-hatfield <47366310+xCthaeh@users.noreply.github.com>
2019-05-06 14:48:41 -0400

    wrapped movie and movie list in routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
-
-import SavedList from './Movies/SavedList';
-import MovieList from './Movies/MovieList';
-import Movie from './Movies/Movie';
+import React, { Component } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import SavedList from "./Movies/SavedList";
+import MovieList from "./Movies/MovieList";
+import Movie from "./Movies/Movie";
 
 export default class App extends Component {
   constructor() {
@@ -22,7 +22,8 @@ export default class App extends Component {
     return (
       <div>
         <SavedList list={this.state.savedList} />
-        <div>Replace this Div with your Routes</div>
+        <Router path="/" component={MovieList} />
+        <Router path="/movie/:id" component={Movie} />
       </div>
     );
   }
